f82a30584cee4dd30396032ce66b2058d135ef2c
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2019-07-17 23:42:58 +0100

    Update mape.R
    example

--- mape.R
+++ mape.R
@@ -1,3 +1,12 @@
 mape <- function(real, pred){
   return(mean(abs(1-pred/real)[real!=0]))
 }
+
+##Random variables example
+
+A<-rnorm(1000,1,0)
+B<-rnorm(1000,1,1)
+
+mape(A,B)
+
+##RModel example...
